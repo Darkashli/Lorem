ec69099b13cbfa16a21044ce72579e01b820b584
Alaa Darkashli <alaa.darkashli@gmail.com>
2020-01-14 17:32:47 +0100

    links configurations + template
    js + css  + template (header, footer, main)

---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>dashboard</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="PHP, HTML, CSS, XML, XHTML, JavaScript">
+	<meta http-equiv="refresh" content="30">
+
+	<script type="text/javascript" src="/node_modules/jquery/dist/jquery.min.js" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="/node_modules/popper.js/dist/popper.min.js" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="/assets/js/custom.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="/assets/fontawesome/all.min.css" />
+	<link rel="stylesheet" type="text/css" href="/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/css/custom.css" />
+</head>
+<body>
+
+</body>
+</html>
